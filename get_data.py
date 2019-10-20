2cd3fa87547f2b971b5ab6eaff5dca3cb6bee7a6
Timotej Giacomelli <timi.giaco@gmail.com>
2019-10-20 12:26:13 +0200

    change
    not working bs4 so i deleted everything

---
+++ get_data.py
@@ -0,0 +1,16 @@
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+import csv
+
+url = 'https://www.avto.net/Ads/results.asp?znamka=&model=&modelID=&tip=katerikoli%20tip&znamka2=&model2=&tip2=katerikoli%20tip&znamka3=&model3=&tip3=katerikoli%20tip&cenaMin=500&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika=0&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999&motortakt=0&motorvalji=0&lokacija=0&sirina=0&dolzina=&dolzinaMIN=0&dolzinaMAX=100&nosilnostMIN=0&nosilnostMAX=999999&lezisc=&presek=0&premer=0&col=0&vijakov=0&EToznaka=0&vozilo=&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000000000&EQ7=1110100120&EQ8=1010000001&EQ9=1000000000&KAT=1010000000&PIA=&PIAzero=&PSLO=&akcija=0&paketgarancije=&broker=0&prikazkategorije=0&kategorija=0&zaloga=10&arhiv=0&presort=3&tipsort=DESC&stran=1&subSORT=2&subTIPSORT=ASC'
+
+try:
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    print(soup)
+    
+except:
+    print("An error occured.")
+
+
