f2a15943e86ce4534c4ccb3c7201b3046b82f7da
Timotej Giacomelli <timi.giaco@gmail.com>
2019-10-20 18:25:21 +0200

    fixing pages
    had a problem with pages and partly fixed it

--- extracting_data.py
+++ extracting_data.py
@@ -9,7 +9,7 @@ pattern = (
     r'\s+'
     r'<a class=".*" href=".*">'
     r'\s+'
-    r'<span>(.*)</span>' #exctrating car name and model
+    r'<span>(?P<name_and_model>.*)</span>' #exctrating car name and model
     r'\s+'
     r'</a>'
     r'\s+'
@@ -17,21 +17,22 @@ pattern = (
     r'\s+'
     r'<ul>'
     r'\s+'
-    r'<li>(.*)</li>' #data of first registration
+    r'<li>(?P<registration>.*)</li>' #data of first registration
     r'\s+'
-    r'<li>(.*)</li><li>(.*)</li><li>(.*)</li>' #(1.) number of km, (2.) type of motor. (3.) type of transmission
+    r'<li>(?P<kilometers>.*)</li><li>(?P<motor>.*)</li><li>(?P<transmission>.*)</li>' #(1.) number of km, (2.) type of motor. (3.) type of transmission
 
 )
 
-cars = []
-count = 1
 
-for i in range(100, 101):
-    content = tools.file_content(f'captured_data/{i}.page.html')
-    print('----------------------NASLEDNJA STRAN--------------------')
-    for match in re.findall(pattern, content):
-        cars.append(match)
-        print(match, count)
-        count += 1
 
-tools.write_json(cars, 'cars.json')        
+def writing_data_json():
+    cars = []
+    count = 1
+    for i in range(len(page_saving.brands)):
+        for j in range(20):
+            content = tools.file_content(f'captured_data/{page_saving.brands[i]}/{page_saving.brands[i]}_page_{j+1}.html')
+            for match in re.finditer(pattern, content):
+                count += 1
+                cars.append(match.groupdict())
+            tools.write_json(cars, f'car_files_json/{page_saving.brands[i]}.json') 
+    print(count)
--- page_saving.py
+++ page_saving.py
@@ -1,8 +1,10 @@
 import tools
 
-url =  'https://www.avto.net/Ads/results.asp?znamka=&model=&modelID=&tip=katerikoli%20tip&znamka2=&model2=&tip2=katerikoli%20tip&znamka3=&model3=&tip3=katerikoli%20tip&cenaMin=100&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika=0&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999&motortakt=0&motorvalji=0&lokacija=0&sirina=0&dolzina=&dolzinaMIN=0&dolzinaMAX=100&nosilnostMIN=0&nosilnostMAX=999999&lezisc=&presek=0&premer=0&col=0&vijakov=0&EToznaka=0&vozilo=&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000000000&EQ7=1110100120&EQ8=1010000001&EQ9=1000000000&KAT=1010000000&PIA=&PIAzero=&PSLO=&akcija=0&paketgarancije=&broker=0&prikazkategorije=0&kategorija=0&zaloga=10&arhiv=0&presort=3&tipsort=DESC&stran={}&subSTAR=303'
+brands = ['Volkswagen', 'BMW', 'Audi', 'Mercedes-Benz', 'Ford']
+url = 'https://www.avto.net/Ads/results.asp?znamka={}&model=&modelID=&tip=katerikoli%20tip&znamka2=&model2=&tip2=katerikoli%20tip&znamka3=&model3=&tip3=katerikoli%20tip&cenaMin=100&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika=0&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999&motortakt=&motorvalji=&lokacija=0&sirina=&dolzina=&dolzinaMIN=&dolzinaMAX=&nosilnostMIN=&nosilnostMAX=&lezisc=&presek=&premer=&col=&vijakov=&EToznaka=&vozilo=&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000000000&EQ7=1000100120&EQ8=1010000001&EQ9=100000000&KAT=1010000000&PIA=&PIAzero=&PSLO=&akcija=&paketgarancije=&broker=&prikazkategorije=&kategorija=&zaloga=&arhiv=&presort=&tipsort=&stran={}'
+num_of_pages = 20 # This is max number of pages available
 
 def save_pages(num_of_pages):
-    for i in range(num_of_pages):
-        tools.make_directory(f'captured_data/{i+1}.page')
-        tools.save_html(url.format(i), f'captured_data/{i+1}.page.html')
+    for i in range(len(brands)):
+        for j in range(num_of_pages):
+            tools.save_html(url.format(brands[i], j), f'captured_data/{brands[i]}/{brands[i]}_page_{j+1}.html')
